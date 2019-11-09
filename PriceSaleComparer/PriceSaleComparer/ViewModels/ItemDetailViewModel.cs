f13350440339c28669adf89e0bd3ff0a0a3b2fcc
MonteShot <faqreg@gmail.com>
2019-11-09 21:36:27 +0200

    Init comit

---
+++ PriceSaleComparer/PriceSaleComparer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PriceSaleComparer.Models;
+
+namespace PriceSaleComparer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
